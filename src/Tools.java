fb656f156baf8f630113a477037cc8bf6b456b4c
Pavel Braginskiy <pavelyay@gmail.com>
2017-03-12 17:56:05 -0700

    Removed an unused method

--- src/Tools.java
+++ src/Tools.java
@@ -50,14 +50,6 @@ public class Tools {
 	{
 		printing=false;
 	}
-
-	public static int[] toC(int pixel) {
-		int alpha = (pixel >> 24) & 0xff;
-		int red = (pixel >> 16) & 0xff;
-		int green = (pixel >> 8) & 0xff;
-		int blue = (pixel) & 0xff;
-		return new int[]{alpha, red, green, blue};
-	}
 	static String toLet(String a) {
 		if(a.matches("[a-zA-Z0-9]"))
 		{
