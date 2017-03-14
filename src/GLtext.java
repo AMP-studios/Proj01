c973b2afe80afd58617f87eb8a506962e39c35f8
Pavel Braginskiy <pavelyay@gmail.com>
2017-03-13 22:19:36 -0700

    Made some of Andrey's code less stupid

--- src/GLtext.java
+++ src/GLtext.java
@@ -29,9 +29,9 @@ public class GLtext {
 	}
 	private void write() throws IOException
 	{
-		String[] br = contents.split("");
-		for(String aBr : br) {
-			createLetter(Tools.toLet(""+aBr), cX, cY, this.size);
+		char[] br = contents.toCharArray();
+		for(char aBr : br) {
+			createLetter(Tools.toLet(aBr), cX, cY, this.size);
 		}
 	}
 
--- src/Tools.java
+++ src/Tools.java
@@ -49,45 +49,45 @@ public class Tools {
 	{
 		printing=false;
 	}
-	static String toLet(String a) {
-		if(a.matches("[a-zA-Z0-9]"))
+	static String toLet(char a) {
+		if(a>'a'&&a<'z'||a>'A'&&a<'Z'||a>'0'&&a<'9')
 		{
-			return "Assets\\Font\\"+a.toUpperCase()+".png";
+			return "Assets\\Font\\"+Character.toUpperCase(a)+".png";
 		}
 		switch (a) {
-			case "`": return "Assets\\Font\\accent.png";
-			case "&": return "Assets\\Font\\and.png";
-			case "*": return "Assets\\Font\\ast.png";
-			case "@": return "Assets\\Font\\at.png";
-			case "\\":return "Assets\\Font\\bslash.png";
-			case "^": return "Assets\\Font\\carrot.png";
-			case ":": return "Assets\\Font\\col.png";
-			case ",": return "Assets\\Font\\comma.png";
-			case "$": return "Assets\\Font\\doll.png";
-			case ".": return "Assets\\Font\\dot.png";
-			case "\"":return "Assets\\Font\\dquot.png";
-			case "=": return "Assets\\Font\\eq.png";
-			case "!": return "Assets\\Font\\exclaim.png";
-			case "/": return "Assets\\Font\\fslash.png";
-			case ">": return "Assets\\Font\\greater.png";
-			case "<": return "Assets\\Font\\less.png";
-			case "-": return "Assets\\Font\\minus.png";
-			case "#": return "Assets\\Font\\num.png";
-			case "%": return "Assets\\Font\\percent.png";
-			case "|": return "Assets\\Font\\pipe.png";
-			case "+": return "Assets\\Font\\plus.png";
-			case "?": return "Assets\\Font\\question.png";
-			case "'": return "Assets\\Font\\quot.png";
-			case "(": return "Assets\\Font\\quote0.png";
-			case ")": return "Assets\\Font\\quote1.png";
-			case "[": return "Assets\\Font\\quote2.png";
-			case "]": return "Assets\\Font\\quote3.png";
-			case "{": return "Assets\\Font\\quote4.png";
-			case "}": return "Assets\\Font\\quote5.png";
-			case ";": return "Assets\\Font\\semicol.png";
-			case " ": return "Assets\\Font\\space.png";
-			case "~": return "Assets\\Font\\tilda.png";
-			case "_": return "Assets\\Font\\under.png";
+			case '`': return "Assets\\Font\\accent.png";
+			case '&': return "Assets\\Font\\and.png";
+			case '*': return "Assets\\Font\\ast.png";
+			case '@': return "Assets\\Font\\at.png";
+			case '\\':return "Assets\\Font\\bslash.png";
+			case '^': return "Assets\\Font\\carrot.png";
+			case ':': return "Assets\\Font\\col.png";
+			case ',': return "Assets\\Font\\comma.png";
+			case '$': return "Assets\\Font\\doll.png";
+			case '.': return "Assets\\Font\\dot.png";
+			case '"':return "Assets\\Font\\dquot.png";
+			case '=': return "Assets\\Font\\eq.png";
+			case '!': return "Assets\\Font\\exclaim.png";
+			case '/': return "Assets\\Font\\fslash.png";
+			case '>': return "Assets\\Font\\greater.png";
+			case '<': return "Assets\\Font\\less.png";
+			case '-': return "Assets\\Font\\minus.png";
+			case '#': return "Assets\\Font\\num.png";
+			case '%': return "Assets\\Font\\percent.png";
+			case '|': return "Assets\\Font\\pipe.png";
+			case '+': return "Assets\\Font\\plus.png";
+			case '?': return "Assets\\Font\\question.png";
+			case '\'': return "Assets\\Font\\quot.png";
+			case '(': return "Assets\\Font\\quote0.png";
+			case ')': return "Assets\\Font\\quote1.png";
+			case '[': return "Assets\\Font\\quote2.png";
+			case ']': return "Assets\\Font\\quote3.png";
+			case '{': return "Assets\\Font\\quote4.png";
+			case '}': return "Assets\\Font\\quote5.png";
+			case ';': return "Assets\\Font\\semicol.png";
+			case ' ': return "Assets\\Font\\space.png";
+			case '~': return "Assets\\Font\\tilda.png";
+			case '_': return "Assets\\Font\\under.png";
 			default:  return "Assets\\Font\\err.png";
 		}
 	}
